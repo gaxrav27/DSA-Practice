783aa00767d84a0cd218d11bb7fcc3ccc3951c3d
Gaurav <234677729+gaxrav27@users.noreply.github.com>
2026-06-21 23:24:46 +0530

    Add median of two sorted arrays solution

---
+++ Data-Structures/02-Binary-Search/median.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+double median(vector<int> &nums1, vector<int> &nums2)
+{
+    int n1 = nums1.size();
+    int n2 = nums2.size();
+    if (n1 > n2)
+        return median(nums2, nums1);
+    int low = 0;
+    int high = n1;
+    while (low <= high)
+    {
+        int cut1 = low + (high - low) / 2;
+        int cut2 = (n1 + n2 + 1) / 2 - cut1;
+        int l1 = (cut1 == 0) ? INT_MIN : nums1[cut1 - 1];
+        int l2 = (cut2 == 0) ? INT_MIN : nums2[cut2 - 1];
+        int r1 = (cut1 == n1) ? INT_MAX : nums1[cut1];
+        int r2 = (cut2 == n2) ? INT_MAX : nums2[cut2];
+        if (l1 <= r2 && l2 <= r1)
+        {
+            if ((n1 + n2) % 2 == 0)
+                return (max(l1, l2) + min(r1, r2)) / 2.0;
+            else
+                return (max(l1, l2));
+        }
+        else if (l1 > r2)
+        {
+            high = cut1 - 1;
+        }
+        else
+            low = cut1 + 1;
+    }
+    return 0.0;
+}
+int main()
+{
+    vector<int> nums1 = {1, 4, 12, 34, 41};
+    vector<int> nums2 = {1, 6, 25, 39, 52};
+    double res = median(nums1, nums2);
+    cout << res;
+    return 0;
+}
